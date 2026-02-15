5637989e724238b6c97c5cf0251fcfcbbe0fcefb
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-02-15 18:51:47 +0400

    setup mongoose connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 const http = require('http');
 
 const app = require('./app');
+const { mongoConnect } = require('./services/mongodb');
+
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-const startServer = () => {
-  server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+const startServer = async () => {
+  await mongoConnect();
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
