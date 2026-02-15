ba763a832f36b2de6dcbd0c685ca3d6c04519df5
Albert Arakelyan <albertarakelyan1998@gmail.com>
2026-02-15 18:43:09 +0400

    configured mongodb connection

---
+++ backend/src/services/mongodb.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+});
+
+const mongoConnect = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+};
+
+const mongoDisconnect = async () => {
+  await mongoose.disconnect();
+};
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
